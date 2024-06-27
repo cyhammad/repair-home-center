9d6fcbdc7b8578893ec21216e54d3a8a7e697766
99-Malik <74320681+99-Malik@users.noreply.github.com>
2024-06-27 03:02:57 -0700

    gtag added

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,6 +16,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+<head>
+        {/* Include gtag.js script */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16624322208" />
+
+        {/* Include gtag.js initialization script */}
+        <Script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16624322208');
+        ` }} />
+               
+
+</head>
+
+      
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
